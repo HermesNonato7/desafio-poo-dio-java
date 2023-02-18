bc1f5785aea7ed8bfb067357dd7914be442d56c5
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-02-18 00:26:09 -0300

    Final do projeto- POO

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
+import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 
 public class Main {
@@ -20,8 +21,43 @@ public class Main {
         mentoria.setDescricao("descriçao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devHermes = new Dev();
+        devHermes.setNome("Hermes");
+        devHermes.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Hermes" + devHermes.getConteudosInscritos());
+
+        devHermes.progredir();
+        devHermes.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Hermes" + devHermes.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Hermes" + devHermes.getConteudosConcluidos());
+        System.out.println("XP" + devHermes.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Lucas" + devLucas.getConteudosInscritos());
+        devLucas.progredir();
+        devLucas.progredir();
+        devLucas.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Lucas" + devLucas.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Lucas" + devLucas.getConteudosConcluidos());
+        System.out.println("XP" + devLucas.calcularTotalXp());
+
+
     }
 }
